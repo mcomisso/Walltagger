042b4d3eea5e489b0ec30011a03a74f6f636a8d3
Matteo Comisso <matteo.comisso@me.com>
2023-09-16 19:18:14 +0100

    Initial Commit

---
+++ Walltagger/WalltaggerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WalltaggerApp.swift
+//  Walltagger
+//
+//  Created by Matteo Comisso on 16/09/23.
+//
+
+import SwiftUI
+
+@main
+struct WalltaggerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
